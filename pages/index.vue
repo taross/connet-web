630589972813d0785f175e87cdc987e483003e78
taross <taross14.selkie@gmail.com>
2018-09-08 17:05:32 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="(post,index) in posts" :key="index">
+      {{ post.id }}：{{ post.message }} @{{ post.userId }}
+    </li>
+  </ul>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    computed: {
+      ...mapGetters({
+        posts: 'getItems'
+      })
+    }
+  }
+</script>
---
+++ store/index.js
@@ -0,0 +1,45 @@
+import Vuex from 'vuex'
+import {
+  INIT
+} from './types'
+import db from '~/plugins/firebaseInit'
+
+const itemRef = db.collection('posts')
+
+const initPlugin = store => store.dispatch(INIT)
+
+const store = () =>
+  new Vuex.Store({
+    state: {
+      itemList: []
+    },
+    mutations: {
+      INIT(state, data) {
+        state.itemList = data
+      }
+    },
+    actions: {
+      INIT({ commit }) {
+        itemRef.get().then(res => {
+          let list = []
+          res.forEach(doc => {
+            let data = {
+              id: doc.id,
+              userId: doc.data().userId,
+              message: doc.data().message,
+            }
+            list.push(data)
+          })
+          commit('INIT', list)
+        })
+      }
+    },
+    getters: {
+      getItems: state => {
+        return state.itemList
+      }
+    },
+    plugins: [initPlugin]
+  })
+
+export default store
